f186fa8ed710e7e449ad7d680d5ffa69fa53f645
Martin Crane <martin.crane@gmail.com>
2017-01-24 19:29:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
 def reformat_languages(languages)
-  # your code here
+
+  data_by_language = {}
+
+    languages.each do |xstyle, style_data|
+      style_data.each do |language_name, language_name_data|
+        data_by_language[language_name] = {}
+      end
+    end
+
+    languages.each do |xstyle, style_data|
+      style_data.each do |language_name, language_name_data|
+
+        if data_by_language[language_name].has_key?(:style)
+          puts "trig"
+          data_by_language[language_name][:style].push(xstyle)
+        else
+          data_by_language[language_name] = {style: [xstyle]}
+        end
+
+        language_name_data.each do |character, caracter_data|
+            data_by_language[language_name][:type] = caracter_data
+        end
+
+      end
+    end
+
+data_by_language
+
 end
